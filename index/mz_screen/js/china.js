f537da9c0b172c5d09564ced3d01380db46ec7c8
Satou <3156701367@qq.com>
2026-03-24 09:01:02 +0800

    修复地图无法显示的bug

--- index/mz_screen/js/china.js
+++ index/mz_screen/js/china.js
@@ -1,4 +1,4 @@
-(function(root, factory) {
+(function (root, factory) {
   if (typeof define === "function" && define.amd) {
     // AMD. Register as an anonymous module.
     define(["exports", "echarts"], factory);
@@ -12,8 +12,8 @@
     // Browser globals
     factory({}, root.echarts);
   }
-})(this, function(exports, echarts) {
-  var log = function(msg) {
+})(this, function (exports, echarts) {
+  var log = function (msg) {
     if (typeof console !== "undefined") {
       console && console.error && console.error(msg);
     }
@@ -26,7 +26,7 @@
     log("ECharts Map is not loaded");
     return;
   }
-  echarts.registerMap("china1", {
+  echarts.registerMap("china", {
     type: "FeatureCollection",
     features: [
       {
